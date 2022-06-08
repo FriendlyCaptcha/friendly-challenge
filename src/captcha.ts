75e2fd57377a3266dc029cce9cfb98cd194e27ec
Merlin <merlinfuchs2001@gmail.com>
2022-06-08 22:35:36 +0200

    reset `hasBeenStarted` on worker error and move `hasBeenStarted` check

--- src/captcha.ts
+++ src/captcha.ts
@@ -142,6 +142,7 @@ export class WidgetInstance {
   }
 
   private onWorkerError(e: any) {
+    this.hasBeenStarted = false;
     this.needsReInit = true;
     this.e.innerHTML = getErrorHTML(this.opts.solutionFieldName, this.lang, "Background worker error " + e.message);
     this.makeButtonStart();
@@ -195,7 +196,6 @@ export class WidgetInstance {
       return;
     }
 
-    this.hasBeenStarted = true;
     const sitekey = this.opts.sitekey || this.e.dataset["sitekey"];
     if (!sitekey) {
       console.error("FriendlyCaptcha: sitekey not set on frc-captcha element");
@@ -224,6 +224,8 @@ export class WidgetInstance {
       this.init(true);
       return;
     }
+
+    this.hasBeenStarted = true;
 
     try {
       this.e.innerHTML = getFetchingHTML(this.opts.solutionFieldName, this.lang);

298a256469d241691f15eb97193f6dd40025bc60
merlinfuchs <merlinfuchs2001@gmail.com>
2026-06-14 12:49:03 +0200

    add rawError to UserFacingError

--- src/puzzle.ts
+++ src/puzzle.ts
@@ -4,9 +4,13 @@ import { NUMBER_OF_PUZZLES_OFFSET, PUZZLE_DIFFICULTY_OFFSET, PUZZLE_EXPIRY_OFFSE
 import { Localization } from "./localization";
 
 class UserFacingError extends Error {
+  rawError: Error;
+
   constructor(message: string) {
     super(message);
     this.name = "UserFacingError";
+    // no underlying error; this is for API compatibility only
+    this.rawError = this;
   }
 }
 
@@ -66,8 +70,8 @@ export async function getPuzzle(urlsSeparatedByComma: string, siteKey: string, l
         }
       }
     } catch (e) {
-      console.error("[FRC Fetch]:", e);
       if (e instanceof UserFacingError) throw e;
+      console.error("[FRC Fetch]:", e);
       const err = new Error(`${lang.text_fetch_error} <a class="frc-err-url" href="${urls[i]}">${urls[i]}</a>`);
       (err as any).rawError = e;
       throw err;
